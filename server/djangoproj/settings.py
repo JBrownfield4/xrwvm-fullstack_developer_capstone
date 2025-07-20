0a87538418f9d0d136644f7c757aed02578879eb
Mark Daniel Callejas <callejasmark63@gmail.com>
2025-07-21 03:40:58 +0800

    Lint project-level urls, settings

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,7 +28,11 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*","https://callejasmark-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+ALLOWED_HOSTS = [
+    "*",
+    "https://callejasmark-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"
+]
+
 CSRF_TRUSTED_ORIGINS = []
 
 REST_FRAMEWORK = {
@@ -143,4 +147,3 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build'),
     os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
