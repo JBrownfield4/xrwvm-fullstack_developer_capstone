2146583027cc7e268a64c1f1378be73c937516a0
JBrownfield4 <justinbrownfield2000@gmail.com>
2025-11-14 11:07:13 -0600

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-#from frontend import src
+# from frontend import src
 
 urlpatterns = [
     path('admin/', admin.site.urls),
