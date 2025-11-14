b04cb9d38465808c30f48a41b921e2de0f6bbb4c
JBrownfield4 <justinbrownfield2000@gmail.com>
2025-11-14 10:40:40 -0600

    Update urls.py

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -1,6 +1,6 @@
 """djangoproj URL Configuration
 
-The `urlpatterns` list routes URLs to views. 
+The `urlpatterns` list routes URLs to views.
 For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
@@ -19,7 +19,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
-from frontend import src
+#from frontend import src
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,8 +30,8 @@ urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('dealers/', TemplateView.as_view(template_name="index.html")),
-    path('dealer/<int:dealer_id>', 
+    path('dealer/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
-    path('postreview/<int:dealer_id>', 
+    path('postreview/<int:dealer_id>',
          TemplateView.as_view(template_name="index.html")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
