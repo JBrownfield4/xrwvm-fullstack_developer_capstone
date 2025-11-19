d781d80b6478946b58f54dc1e6154f2aadb9c90a
JBrownfield4 <justinbrownfield2000@gmail.com>
2025-11-19 11:31:56 -0600

    Update views.py

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,12 +1,12 @@
 # Uncomment the required imports before adding the code
 
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.models import User
-from django.shortcuts import get_object_or_404, render, redirect
+# from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth import logout
-from django.contrib import messages
-from datetime import datetime
+# from django.contrib import messages
+# from datetime import datetime
 from django.http import JsonResponse
 from django.contrib.auth import login, authenticate
 import logging
@@ -20,7 +20,7 @@ from .restapis import post_review
 
 
 # Get an instance of a logger
-logger = logging.getLogger(__name__)
+logger=logging.getLogger(__name__)
 
 
 # Create your views here.
@@ -29,64 +29,64 @@ logger = logging.getLogger(__name__)
 @csrf_exempt
 def login_user(request):
     # Get username and password from request.POST dictionary
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
+    data=json.loads(request.body)
+    username=data['userName']
+    password=data['password']
     # Try to check if provide credential can be authenticated
-    user = authenticate(username = username, password = password)
-    data = {"userName": username}
+    user=authenticate(username=username, password=password)
+    data={"userName": username}
     if user is not None:
         # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data={"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
 
 # Create a `logout_request` view to handle sign out request
 def logout_request(request):
-    logout(request) # Terminate user session
-    data = {"userName": ""} # Return empty username
+    logout(request)  # Terminate user session
+    data={"userName": ""}  # Return empty username
     return JsonResponse(data)
-    
+
 
 # Create a `registration` view to handle sign up request
 @csrf_exempt
 def registration(request):
-    context = {}
+    context={}
 
     # Load JSON data from the request body
-    data = json.loads(request.body)
-    username = data['userName']
-    password = data['password']
-    first_name = data['firstName']
-    last_name = data['lastName']
-    email = data['email']
-    username_exist = False
-    email_exist = False
+    data=json.loads(request.body)
+    username=data['userName']
+    password=data['password']
+    first_name=data['firstName']
+    last_name=data['lastName']
+    email=data['email']
+    username_exist=False
+    email_exist=False
     try:
         # Check if user already exists
         User.objects.get(username = username)
         username_exist = True
-    except Exception as f:
+    except:
         # If not, simply log this is a new user
         logger.debug("{} is new user".format(username))
 
     # If it is a new user
     if not username_exist:
         # Create user in auth_user table
-        user = User.objects.create_user(
-                                        username = username,
-                                        first_name = first_name,
-                                        last_name = last_name,
-                                        password = password,
-                                        email = email
+        user=User.objects.create_user(
+                                        username=username,
+                                        first_name=first_name,
+                                        last_name=last_name,
+                                        password=password,
+                                        email=email
                                         )
         # Login the user and redirect to list page
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data={"userName": username, "status": "Authenticated"}
         return JsonResponse(data)
     else:
-        data = {"userName": username, "error": "Already Registered"}
+        data={"userName": username, "error": "Already Registered"}
         return JsonResponse(data)
 
 
@@ -96,12 +96,12 @@ def registration(request):
 # ...
 # Update the `get_dealerships` render list of dealerships all by default,
 # particular state if state is passed
-def get_dealerships(request, state = "All"):
-    if (state == "All"):
-        endpoint = "/fetchDealers"
+def get_dealerships(request, state="All"):
+    if (state=="All"):
+        endpoint="/fetchDealers"
     else:
-        endpoint = "/fetchDealers/"+state
-    dealerships = get_request(endpoint)
+        endpoint="/fetchDealers/"+state
+    dealerships=get_request(endpoint)
     return JsonResponse({"status": 200, "dealers": dealerships})
 
 
@@ -143,7 +143,7 @@ def add_review(request):
         try:
             response = post_review(data)
             return JsonResponse({"status": 200})
-        except Exception as e:
+        except:
             return JsonResponse({"status": 401,
                                  "message": "Error in posting review"})
     else:
