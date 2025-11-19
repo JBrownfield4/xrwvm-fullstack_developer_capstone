d924ce4348f9c531ff5ee3edc5b0244a0119a697
JBrownfield4 <justinbrownfield2000@gmail.com>
2025-11-19 11:08:48 -0600

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,7 +1,7 @@
 # Uncomment the following imports before adding the Model code
 
 from django.db import models
-from django.utils.timezone import now
+# from django.utils.timezone import now
 from django.core.validators import MaxValueValidator, MinValueValidator
 
 
@@ -15,6 +15,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+    
     def __str__(self):
         return self.name  # Return the name as the string representation
 
@@ -30,7 +31,9 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='models')  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake,
+                                 on_delete=models.CASCADE,
+                                 related_name='models')  # Many-to-One relationship
     dealer_id = models.IntegerField()  # Refers to dealer in Cloudant DB
     name = models.CharField(max_length=100)
     CAR_TYPES = [
@@ -49,4 +52,4 @@ class CarModel(models.Model):
     )
 
     def __str__(self):
-        return f"{self.car_make.name} {self.name} ({self.year})" 
+        return f"{self.car_make.name} {self.name} ({self.year})"
